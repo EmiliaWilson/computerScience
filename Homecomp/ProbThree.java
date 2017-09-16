fb114e3feee4ea0a0185b65b94b7574ec1e92e71
Jake-R-W <31546309+Jake-R-W@users.noreply.github.com>
2017-09-16 16:58:47 -0400

    workingProbThree

--- Homecomp/ProbThree.java
+++ Homecomp/ProbThree.java
@@ -3,24 +3,26 @@ public class ProbThree
     public static void main (String[] args)
     {
         
-        int[] numChecker = {1, 3, 5, 3, -1, 0, 12, 5};
+        int[] numChecker = {1, 3, 5, 4, 1, -1, 5, 21};
         boolean numSame;
-        for (int i : numChecker) 
+        for (int i = 0; i < numChecker.length; i++) 
         {
             numSame = false;
-            for (int p : numChecker)
+            
+            for (int p = 0; p < numChecker.length; p++)
             {
-                if ( i == p && i.length != p.length) 
+                if ( i != p && numChecker[i] == numChecker[p] && p <= i) 
                 {
                   numSame = true;  
                 }
             }
+            
             if ( numSame == true)
             {
-                System.out.println ( i + " NOT first instance");
+                System.out.println ( numChecker[i] + " NOT first instance");
             } else 
             {
-                System.out.println( i + " first instance");
+                System.out.println( numChecker[i] + " first instance");
             }
         }
     }
